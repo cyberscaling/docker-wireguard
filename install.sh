61632f5b50282e95d8e91b6284e0d5164c4403a0
Ubuntu <ubuntu@ip-172-26-3-228.eu-west-3.compute.internal>
2020-05-26 22:41:06 +0000

    update script

--- install.sh
+++ install.sh
@@ -12,10 +12,11 @@ sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-pro
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-
-sudo apt install -y docker-ce
-
-sudo apt install -y docker-compose
+sudo apt update
+sudo apt install -y docker-ce &&
+sudo apt install -y docker-compose &&
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose &&
+sudo chmod +x /usr/bin/docker-compose
 
 sudo systemctl status docker
 
