6c2f179327ae0e9fb07076f95da61cbdf9a20d76
pierre Siccardi <pierre.siccardi@gmail.com>
2020-05-28 18:28:11 +0200

    fix try to Dockerfile nextDns

--- nextdns/run.sh
+++ nextdns/run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+
 set -e
 
 [ -n "${NEXTDNS_ID}" ] &&  NEXTDNS_ARGUMENTS+=" -config ${NEXTDNS_ID}"
 
-./nextdns run ${NEXTDNS_ARGUMENTS}
+./nextdns run ${NEXTDNS_ARGUMENTS} &
+
+echo $$
+# -$$: kill process group (parent and children)
+#trap 'trap - TERM; kill 0' TERM
+#trap 'trap - INT TERM; kill 0' INT TERM
+
+trap 'trap - TERM; kill -s TERM -- -$$' TERM
+
+tail -f /dev/null & wait
+
+exit 0
+
+
