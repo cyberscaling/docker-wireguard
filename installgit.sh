991c0cb69d90885224cecd8ea141cb4015e98f49
pierre Siccardi <pierre.siccardi@gmail.com>
2020-07-05 19:08:31 +0200

    restart script

--- installgit.sh
+++ installgit.sh
@@ -2,3 +2,7 @@ sudo apt update && apt install -y git &&
 git clone https://github.com/darksip/docker-wireguard.git &&
 cd docker-wireguard &&
 bash ./install_debian_root.sh
+
+#----- After reboot
+cd docker-wireguard &&
+bash ./install_docker.sh
---
+++ restart_wireguard.sh
@@ -0,0 +1,3 @@
+docker exec -it wireguard \
+    docker-compose down && docker-compose up -d
+    
