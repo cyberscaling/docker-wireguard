378ee8057528c4b5b77245e85ef734b3f27594ae
pierre Siccardi <pierre.siccardi@gmail.com>
2020-07-05 18:59:42 +0200

    wg0 en 10.0.0

--- installgit.sh
+++ installgit.sh
@@ -1,2 +1,4 @@
 sudo apt update && apt install -y git &&
-git clone https://github.com/darksip/docker-wireguard.git
+git clone https://github.com/darksip/docker-wireguard.git &&
+cd docker-wireguard &&
+bash ./install_debian_root.sh
